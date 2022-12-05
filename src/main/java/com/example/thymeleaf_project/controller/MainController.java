8f0c613cc814d4b83cc4db7acd0af3883c5b8af1
sumayyayellow <sumayyayellow@gmail.com>
2022-12-04 23:28:40 -0600

    add something

--- src/main/java/com/example/thymeleaf_project/controller/MainController.java
+++ src/main/java/com/example/thymeleaf_project/controller/MainController.java
@@ -1,15 +1,18 @@
-package controller;
+package com.example.thymeleaf_project.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/home")
 public class MainController {
-    @GetMapping("")
+    @RequestMapping("/home")
    public String homePage(){
         System.out.println("homepage called");
        return "home";
+   }
+    @RequestMapping("/addProgrammer")
+   public String addProgrammer(){
+      return "";
    }
 }
